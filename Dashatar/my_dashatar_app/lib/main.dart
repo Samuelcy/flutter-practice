1bcac0568b5e288954b43a5c2e1663f9dbfc5b8c
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-07-05 15:06:58 -1000

    Add dashatar styling

--- Dashatar/my_dashatar_app/lib/main.dart
+++ Dashatar/my_dashatar_app/lib/main.dart
@@ -1,22 +1,75 @@
 // ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.black,
+        // backgroundColor: Colors.black,
         appBar: AppBar(
-          backgroundColor: Colors.black,
+          // backgroundColor: Colors.black,
           centerTitle: true,
           title: Text("My Dashatar App"),
         ),
-        body: Center(
-          child: Image.asset("images/myDashatar.png"),
-        ),
+        body: Stack(children: [
+          Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.teal,
+              child: Image.asset("images/myDashatar.png")),
+          Positioned(
+            left: 10.0,
+            child: Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.brown,
+                child: Image.asset("images/myDashatar.png")),
+          ),
+          // Container(
+          //     height: 100.0,
+          //     width: 100.0,
+          //     color: Colors.red,
+          //     child: Image.asset("images/myDashatar.png")),
+          // Container(
+          //     height: 100.0,
+          //     width: 100.0,
+          //     color: Colors.teal,
+          //     child: Image.asset("images/myDashatar.png")),
+          // Container(
+          //     height: 100.0,
+          //     width: 100.0,
+          //     color: Colors.brown,
+          //     child: Image.asset("images/myDashatar.png")),
+          // Container(
+          //     height: 100.0,
+          //     width: 100.0,
+          //     color: Colors.red,
+          //     child: Image.asset("images/myDashatar.png")),
+          // Container(
+          //     height: 100.0,
+          //     width: 100.0,
+          //     color: Colors.teal,
+          //     child: Image.asset("images/myDashatar.png")),
+          // Container(
+          //     height: 100.0,
+          //     width: 100.0,
+          //     color: Colors.brown,
+          //     child: Image.asset("images/myDashatar.png")),
+          // Container(
+          //     height: 100.0,
+          //     width: 100.0,
+          //     color: Colors.red,
+          //     child: Image.asset("images/myDashatar.png")),
+        ]),
       ),
-    ),
-  );
+    );
+  }
 }
