d09adaf47da4dda520c292a6310d41b1bbf091e7
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-07-05 20:37:10 -1000

    State management - basics

--- Dashatar/my_dashatar_app/lib/main.dart
+++ Dashatar/my_dashatar_app/lib/main.dart
@@ -1,74 +1,67 @@
 // ignore_for_file: prefer_const_constructors
+import 'dart:math';
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Color> colours = [
+    Colors.white,
+    Colors.teal,
+    Colors.black,
+    Colors.pink,
+    Colors.red,
+    Colors.purple
+  ];
+  int appBar = 0;
+  int scaffold = 0;
+  Random random = new Random();
+  List<String> sounds = ["Kick.wav", 'Ride.wav', 'Snare.wav', 'Tom.wav'];
+  int soundPosition = 0;
+  final player = AudioPlayer();
+
+  // String kick = "Kick.wav";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        // backgroundColor: Colors.black,
         appBar: AppBar(
-          // backgroundColor: Colors.black,
+          backgroundColor: colours[appBar],
           centerTitle: true,
-          title: Text("My Dashatar App"),
-        ),
-        body: Stack(children: [
-          Container(
-              height: 100.0,
-              width: 100.0,
-              color: Colors.teal,
-              child: Image.asset("images/myDashatar.png")),
-          Positioned(
-            left: 10.0,
-            child: Container(
-                height: 50.0,
-                width: 50.0,
-                color: Colors.brown,
-                child: Image.asset("images/myDashatar.png")),
+          title: IconButton(
+            icon: Icon(Icons.refresh, color: Colors.red),
+            onPressed: () {
+              soundPosition = random.nextInt(4);
+            },
           ),
-          // Container(
-          //     height: 100.0,
-          //     width: 100.0,
-          //     color: Colors.red,
-          //     child: Image.asset("images/myDashatar.png")),
-          // Container(
-          //     height: 100.0,
-          //     width: 100.0,
-          //     color: Colors.teal,
-          //     child: Image.asset("images/myDashatar.png")),
-          // Container(
-          //     height: 100.0,
-          //     width: 100.0,
-          //     color: Colors.brown,
-          //     child: Image.asset("images/myDashatar.png")),
-          // Container(
-          //     height: 100.0,
-          //     width: 100.0,
-          //     color: Colors.red,
-          //     child: Image.asset("images/myDashatar.png")),
-          // Container(
-          //     height: 100.0,
-          //     width: 100.0,
-          //     color: Colors.teal,
-          //     child: Image.asset("images/myDashatar.png")),
-          // Container(
-          //     height: 100.0,
-          //     width: 100.0,
-          //     color: Colors.brown,
-          //     child: Image.asset("images/myDashatar.png")),
-          // Container(
-          //     height: 100.0,
-          //     width: 100.0,
-          //     color: Colors.red,
-          //     child: Image.asset("images/myDashatar.png")),
-        ]),
+        ),
+        backgroundColor: colours[scaffold],
+        body: MaterialButton(
+          child: Image.asset('images/myDashatar.png'),
+          onPressed: () {
+            setState(() {
+              // player.play(AssetSource(kick));
+              player.play(AssetSource(sounds[soundPosition]));
+              soundPosition =
+                  (soundPosition + 1) % sounds.length; // Cycle through sounds
+              appBar = random.nextInt(6);
+              scaffold = random.nextInt(6);
+            });
+          },
+        ),
       ),
     );
   }
